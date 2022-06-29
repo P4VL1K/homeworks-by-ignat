e5027331b41457697e618ee20c1eabab856a2fbb
budch4nin <pashoo0ook@mail.ru>
2022-06-29 16:43:50 +0300

    9 homeworks

--- src/p2-homeworks/h9/HW9.tsx
+++ src/p2-homeworks/h9/HW9.tsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import Clock from './Clock'
+import s from './HW9.module.css'
+
+
 
 function HW9() {
     return (
-        <div>
-            <hr/>
-            homeworks 9
-
+        <div className={s.main}>
+            <h3 className={s.title}>Homeworks 9</h3>
             {/*should work (должно работать)*/}
             <Clock/>
 
-            <hr/>
             {/*для личного творчества, могу проверить*/}
             {/*<AlternativeClock/>*/}
-            <hr/>
+
         </div>
     )
 }
